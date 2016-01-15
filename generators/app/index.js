6953cca302a4089d8d64779fabef000c20a9aee9
Neil de Carteret <ndecarteret@sportingindex.com>
2016-01-15 07:26:13 +0000

    a bit more

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,73 @@
 var generators = require('yeoman-generator');
+var gitEmail = require("git-user-email");
+var gitUsername = require("git-user-name");
 
-module.exports = generators.NamedBase.extend();
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    //this.argument("name", {type: "string", required: true})
+  },
+
+  initializing: function () {
+
+  },
+
+  prompting: function () {
+    var done = this.async();
+    this.prompt([{
+        type: "input",
+        name: "name",
+        message: "Package name",
+        default: this.appname
+      },
+      // {
+      //   type: "input",
+      //   name: "email",
+      //   message: "Your email address",
+      //   default: gitEmail(),
+      //   store: true,
+      // },
+      // {
+      //   type: "input",
+      //   name: "username",
+      //   message: "Your name",
+      //   default: gitUsername(),
+      //   store: true,
+      // },
+    ], function (answers) {
+      this.log("name: "+ answers.name)
+      this.answers = answers;
+      done()
+    }.bind(this));
+  },
+
+  configuring: function () {
+
+  },
+
+  default: function () {
+
+  },
+
+  writing: function () {
+    var package = this.fs.readJSON("package.json");
+    package.name = this.answers.name;
+    package.main = "src/" + name + ".js"
+    this.fs.writeJSON("package.json", package)
+  },
+
+  conflicts: function () {
+
+  },
+
+  install: function () {
+
+  },
+
+  end: function () {
+
+  },
+
+
+
+});
