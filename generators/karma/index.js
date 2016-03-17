4be813b8c99beaf0e84aa04b6e77fc2938f3de55
Neil de Carteret <git@lumphammer.com>
2016-03-17 15:52:11 +0000

    add babelify for karma dev deps

--- generators/karma/index.js
+++ generators/karma/index.js
@@ -10,15 +10,18 @@ module.exports = generators.Base.extend({
   writing: function () {
     var package = this.fs.readJSON(this.destinationPath("package.json"));
     package.devDependencies = package.devDependencies || {}
-    package.devDependencies["browserify"] = "^13.0.0";
-    package.devDependencies["envify"] = "^3.4.0";
-    package.devDependencies["karma"] = "^0.13.21";
-    package.devDependencies["karma-browserify"] = "^5.0.1";
-    package.devDependencies["karma-firefox-launcher"] = "^0.1.7";
-    package.devDependencies["karma-mocha"] = "^0.2.2";
-    package.devDependencies["karma-notify-reporter"] = "^0.1.1";
-    package.devDependencies["karma-teamcity-reporter"] = "^0.2.1";
-    package.devDependencies["watchify"] = "^3.7.0";
+    package.devDependencies["browserify"] = "^13.0.0"
+    package.devDependencies["envify"] = "^3.4.0"
+    package.devDependencies["karma"] = "^0.13.21"
+    package.devDependencies["karma-browserify"] = "^5.0.1"
+    package.devDependencies["karma-firefox-launcher"] = "^0.1.7"
+    package.devDependencies["karma-mocha"] = "^0.2.2"
+    package.devDependencies["karma-notify-reporter"] = "^0.1.1"
+    package.devDependencies["karma-teamcity-reporter"] = "^0.2.1"
+    package.devDependencies["watchify"] = "^3.7.0"
+    if (this.config.get("babel")) {
+      package.devDependencies["babelify"] = "^7.2.0"
+    }
     package.scripts = package.scripts || {}
     package.scripts.test = "karma start"
     this.fs.writeJSON(this.destinationPath("package.json"), package)
