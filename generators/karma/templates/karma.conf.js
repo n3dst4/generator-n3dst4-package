6667a776e23a28d8f82ee9b886a488aa002effcb
Neil de Carteret <git@lumphammer.com>
2017-04-21 14:20:14 +0100

    karma can safely be downgraded to warn-level logging

--- generators/karma/templates/karma.conf.js
+++ generators/karma/templates/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     // CLI --log-level debug
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_WARN,
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
